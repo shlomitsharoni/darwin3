db64aaeacecddedb581401e5742fc5acbcbb6671
shlomitsharoni <ssharoni@mit.edu>
2023-05-31 14:59:10 -0400

    Add RQ diagnostics

--- pkg/darwin/DARWIN_DIAGS.h
+++ pkg/darwin/DARWIN_DIAGS.h
@@ -33,7 +33,7 @@ C Contains indices into diagnostics array
       integer iCChl, iNChl, iNPho, iNSyn, iNPrn, iNRNA
       integer iNDNA, iNSTO, iNEXC
       integer iPRNA, iPDNA, iPTHY, iPCON, iPSTO, iPEXC
-      integer iFPHO, iFSTO, iFEXC
+      integer iFPHO, iFSTO, iFEXC, iRQ_N, iY_RQ
       integer iMODE, iFe_C, iexQc, iPCg
 #endif
       integer iConsO2
@@ -112,7 +112,9 @@ C Contains indices into diagnostics array
       PARAMETER(iFPHO=   iPEXC+nPhoto)
       PARAMETER(iFSTO=   iFPHO+nPhoto)
       PARAMETER(iFEXC=   iFSTO+nPhoto)
-      PARAMETER(iaMacro= iFEXC+nPhoto)
+      PARAMETER(iRQ_N=   iFEXC+nPhoto)
+      PARAMETER(iY_RQ=   iRQ_N+nPhoto)
+      PARAMETER(iaMacro= iY_RQ+nPhoto)
 #else
       PARAMETER(iaMacro= 22)
 #endif
