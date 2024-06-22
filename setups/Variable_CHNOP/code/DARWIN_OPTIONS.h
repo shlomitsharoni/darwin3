e25cedd0da6dd3473670f9733facb00e55f5bd8f
Shlomit Sharoni <ssharoni@mit.edu>
2024-06-22 12:19:24 +0300

    add field links to Variable_chnop/data.ptracers

--- setups/Variable_CHNOP/code/DARWIN_OPTIONS.h
+++ setups/Variable_CHNOP/code/DARWIN_OPTIONS.h
@@ -58,6 +58,7 @@ C enable macromolecular growth code; requires N, P and Fe quotas
 #define DARWIN_MACROMOLECULAR_GROWTH
 
 C enable mvariable C:H:N:O:P code; requires MACROMOLECULAR_GROWTH
+C does not work yet with Bacteria
 #define DARWIN_ALLOW_VARIABLE_CHNOP
 
 
